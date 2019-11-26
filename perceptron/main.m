0ecd1766a4cdfc44422a9bc8fa854eb9fdd0fc89
Theys96 <thijs@thijshavinga.nl>
2019-11-26 14:46:07 +0100

    started on rosenblatt learning

--- perceptron/main.m
+++ perceptron/main.m
@@ -1,5 +1,25 @@
 function void = main
+    N      = 2;
+    P      = 4;
+    n_max  = 10;
+    
     % rng('default');
-    [vecs, labels] = generate(4,2);
+    [vecs, labels] = generate(P, N);
+    
+    w = zeros([N 1]);
+    mu = 1;
+    epoch = 1;
+    for sweep = 1:n_max
+        for mu = 1:P
+            cur_sample = vecs(mu,:)';
+            E = dot(w, cur_sample) * labels(mu);
+            if E <= 1
+                w = w + (cur_sample/N * labels(mu));
+            end
+            epoch = epoch + 1;
+        end
+    end
+    
+    w
     plot_data(vecs, labels);
 end
